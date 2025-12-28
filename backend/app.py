701ed0b5955bdb3a3f33e4d93a292628c9ea6dd3
Soubhagya Raha <soubhagyaraha24@gmail.com>
2025-12-28 23:12:42 +0530

    Deployment

--- backend/app.py
+++ backend/app.py
@@ -22,5 +22,5 @@ def predict_ct():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const BACKEND_URL = "http://127.0.0.1:5000/predict";
+const BACKEND_URL = "https://lung-cancer-detection-vit.onrender.com/predict";
 
 function App() {
   const [file, setFile] = useState(null);
